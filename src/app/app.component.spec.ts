9ce4bab54115147825ff0b40c44d9cad55a4e8bf
Weiqifan1 <cmlykke@hotmail.com>
2025-01-25 12:08:58 +0100

    renamed project in sourcefiles

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'recolibre' title`, () => {
+  it(`should have the 'memolibre' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('recolibre');
+    expect(app.title).toEqual('memolibre');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, recolibre');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, memolibre');
   });
 });
