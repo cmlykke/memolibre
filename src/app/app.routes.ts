4054ebcb6ae820a9c1f52a7dfa794cf31f7311ae
Weiqifan1 <cmlykke@hotmail.com>
2025-02-26 19:28:23 +0100

    2025-02-26_1928 added settings components to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import {CreateDeckComponent} from './create-deck-component/create-deck-component.component';
 import {LoadUserDataComponent} from './load-user-data/load-user-data.component';
 import {PracticePageComponent} from './practice-page/practice-page.component';
+import {AppSettingsComponent} from './app-settings/app-settings.component';
+import {PracticeSettingsComponent} from './practice-settings/practice-settings.component';
 
 export const routes: Routes = [{
   path: '',
@@ -16,5 +18,11 @@ export const routes: Routes = [{
 }, {
   path: 'practice-page',
   component: PracticePageComponent
+}, {
+  path: 'practice-settings',
+  component: PracticeSettingsComponent
+}, {
+  path: 'app-settings',
+  component: AppSettingsComponent
 }
 ];
